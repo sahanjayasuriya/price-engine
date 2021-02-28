cbbe0ab8c985132bf3fb3bfce31368f95f13fcbc
Sahan Jayasuriya <sahanjayasuriya@hotmail.com>
2021-02-28 13:35:37 +0530

    Added docker-compose.yml Added data.sql to insert initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.parameter (id, carton_discount, discount_eligible_cartons, labor_percentage) VALUES (1, 0.1, 3, 0.3);
+
+INSERT INTO public.product (id, label, price, product_name, units) VALUES (1, 1, 175, 'Penguin-ears', 20);
+INSERT INTO public.product (id, label, price, product_name, units) VALUES (2, 0, 825, 'Horseshoe', 5);
